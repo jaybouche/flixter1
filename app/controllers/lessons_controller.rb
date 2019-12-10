fd20daf5890391f7329a452c88e5ab475b986a91
jaybouche <jaybouche@gmail.com>
2019-12-10 22:22:44 +0000

    29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+# before_action :require_authorized_for_current_lesson
+
+
   def show
+  
   end
 
+  
+
   private
+
 
   helper_method :current_lesson
   def current_lesson
